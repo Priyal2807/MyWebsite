34fc60cfec878875dab6d17fbf7bcdd6e145a62f
Priyal Sobti <priyalsobti@gmail.com>
2019-08-29 23:36:58 +0530

    made changes for project.ejs

--- public/assets/website.js
+++ public/assets/website.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
     $('img').addClass('myImg');
+    $('.counter').counterUp({
+      delay: 70,
+ time: 1000
+    });
 });
 $(function(){
   $('.main-menu').mouseover(function(){
@@ -36,8 +40,45 @@ $('img').removeClass('myImgafter');
 $('img').addClass('myImg');
 
   });
-  // if($($(this).selector + ":hover").length > 0){
-  //   alert('hello');
-  //   $('.about-me').css('margin-left','40vh');
-  // }
+
+
+});
+$(function(){
+  $('.projectSlideShow > div:gt(0)').hide();
+  $('.projectEmpty').append($('.project1').html()).show();
+  var count = 0;
+function slideshow(){
+          if(count === 4){
+              $('.projectEmpty').empty();
+              $('.projectEmpty').append($('.project1').html()).show();
+              $('.project1').addClass('active');
+              count = 0;
+          }
+      else{
+          var activeSlide = $('.projectSlideShow').find('.active');
+          $('.projectEmpty').empty();
+          $('.projectEmpty').append(activeSlide.next().html()).show();
+          if(count!=3){
+          $('.projectSlideShow').find('.active').next().addClass('active');}
+
+          activeSlide.removeClass('active').hide();
+
+          count++;
+        }
+
+    }
+    function cinterval(){
+      initList = setInterval(slideshow, 3000) ;
+    }
+  $('.projectSlideShow').mouseover(function(){
+    clearInterval(initList);
+ }).mouseout(function(){
+    cinterval();
+
+ });
+ cinterval();
+
+  //
+  // setInterval(slideshow,3000);
+
 });
